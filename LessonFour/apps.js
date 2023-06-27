fff897d4f92f7bc72d1a74ad5a8192f10fd86750
VIJAYADASS <Vijaydass0702@gmail.com>
2023-06-27 19:41:48 +0530

    fetch call in app

--- LessonFour/apps.js
+++ LessonFour/apps.js
@@ -1,11 +1,21 @@
 var btntranslate = document.querySelector("#btn-translate");
-
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#Output");
  
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" 
+/*fetch()*/
+function gettranslation(text){
+    return serverURL + "?" + "text=" + text
+}
 
 function clickEventHandler() {
-    outputDiv.innerText = "Talk me  "+  txtInput.value;
+    //outputDiv.innerText = "Talk me  "+  txtInput.value;
+    var inputText = txtInput.value; //ip
+
+    // call server
+    fetch(gettranslation(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
 };
 
 btntranslate.addEventListener ("click", clickEventHandler) 

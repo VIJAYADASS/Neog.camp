9fb049182f8e6cd48bc015c955e5df94dbfa1afa
VIJAYADASS <Vijaydass0702@gmail.com>
2023-06-28 11:42:29 +0530

    add the server url in dolan and add the comments, error handler

--- LessonFour/apps.js
+++ LessonFour/apps.js
@@ -1,21 +1,33 @@
-var btntranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#Output");
+var btntranslate = document.querySelector("#btn-translate"); // get button
+var txtInput = document.querySelector("#txt-input"); // get input
+var outputDiv = document.querySelector("#Output"); //get output
  
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" 
+
+var serverURL = "https://api.funtranslations.com/translate/dolan.json" // we want to talk to the dolan server
 /*fetch()*/
-function gettranslation(text){
-    return serverURL + "?" + "text=" + text
+function gettranslation(input){
+    return serverURL + "?" + "text=" + input // serverurl + query  + key= + value
+}
+
+/*Error handler*/
+function errorhandler(error) {
+    console.log("error occured", error);
+    alert("Try again later")
 }
 
 function clickEventHandler() {
-    //outputDiv.innerText = "Talk me  "+  txtInput.value;
+     
     var inputText = txtInput.value; //ip
 
     // call server
     fetch(gettranslation(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    //.then(json => console.log(json.contents.translated))
+    .then(abc => {
+        var translatedText = abc.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
 };
 
 btntranslate.addEventListener ("click", clickEventHandler) 
